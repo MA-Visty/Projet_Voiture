7aa49bda772b47438dfde4094e1c0639df2bb10a
KouptchN <121085575+KouptchN@users.noreply.github.com>
2023-04-18 16:34:48 +0200

    Nomenclature motorDC

--- files/mClass/motorDC.py
+++ files/mClass/motorDC.py
@@ -3,41 +3,42 @@ import mClass.PCA9685 as p
 import time    # Import necessary modules
 
 class DC:
-	def __init__(self, _pina, _pinb, _en):
-		self.pina=_pina
-		self.pinb=_pinb
+	def __init__(self, _pinA, _pinB, _en):
+		self.pinA=_pinA
+		self.pinB=_pinB
 		self.en=_en
-		self.pins=[_pina,_pinb]
+		self.pins=[_pinA,_pinB]
 		busnum=None
-		global forward, backward
+		
+		
 		global pwm
 		if busnum == None:
 			pwm = p.PWM()                  # Initialize the servo controller.
 		else:
 			pwm = p.PWM(bus_number=busnum) # Initialize the servo controller.
 		pwm.frequency = 60
-		forward = 'True'
+		self.forwardB = 'True'
 		GPIO.setwarnings(False)
 		GPIO.setmode(GPIO.BCM)        # Number GPIOs by its physical location
 		try:
 			for line in open("config"):
 				if line[0:8] == "forward0":
-					forward = line[11:-1]
+					self.forwardB = line[11:-1]
 		except:
 			pass
-		if forward == 'True':
-			backward = 'False'
-		elif forward == 'False':
-			backward = 'True'
+		if self.forwardB == 'True':
+			self.backwardB = 'False'
+		elif self.forwardB == 'False':
+			self.backwardB = 'True'
 		for pin in self.pins:
 			GPIO.setup(pin, GPIO.OUT)   # Set all pins' mode as output
 	def motor(self,x):
 		if x == 'True':
-			GPIO.output(self.pina, GPIO.LOW)
-			GPIO.output(self.pinb, GPIO.HIGH)
+			GPIO.output(self.pinA, GPIO.LOW)
+			GPIO.output(self.pinB, GPIO.HIGH)
 		elif x == 'False':
-			GPIO.output(self.pina, GPIO.HIGH)
-			GPIO.output(self.pinb, GPIO.LOW)
+			GPIO.output(self.pinA, GPIO.HIGH)
+			GPIO.output(self.pinB, GPIO.LOW)
 		else:
 			print ('Config Error')
 	def setSpeed(self,speed):
@@ -45,9 +46,9 @@ class DC:
 		print ('speed is: ', speed)
 		pwm.write(self.en, 0, speed)
 	def backward(self):
-		self.motor(forward)
+		self.motor(self.forwardB)
 	def forward(self):
-		self.motor(backward)
+		self.motor(self.backwardB)
 	def stop(self):
 		for pin in self.pins:
 			GPIO.output(pin, GPIO.LOW)
