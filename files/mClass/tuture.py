521c1cff1ff3d96157967847d8180550ba9f9182
MA-Visty <mahalex1@gmail.com>
2023-04-18 16:17:13 +0200

    Circle

--- files/mClass/tuture.py
+++ files/mClass/tuture.py
@@ -8,62 +8,84 @@ from mClass.sensorUltraSonic import UltraSonic
 
 class Car:
 	def __init__(self):
-		try:
-			GPIO.setwarnings(False)
-			GPIO.setmode(GPIO.BCM)
-			"""
-				PIN motor 1 :
-				17
-				18
-				4
-				PIN motor 2 :
-				27
-				15
-				5
-				Ultrasonic Left :
-				23
-				21
-				Ultrasonic Front :
-				31
-				29
-				Ultrasonic Right :
-				37
-				35
-				Infrared :
-				20
-			"""
-			# Motor Left
-			self.mL = DC(10, 24, 4)  # 19->10 ; 18->24 ; 
-			# Motor Right
-			self.mR = DC(17, 23, 5)  # 17->17 ; 16->23 ;
-			# Servomotor
-			self.direction = PAPA()
+		GPIO.setwarnings(False)
+		GPIO.setmode(GPIO.BCM)
+		"""
+			PIN motor 1 :
+			17
+			18
+			4
+			PIN motor 2 :
+			27
+			15
+			5
+			Ultrasonic Left :
+			23
+			21
+			Ultrasonic Front :
+			31
+			29
+			Ultrasonic Right :
+			37
+			35
+			Infrared :
+			20
+		"""
+		# Motor Left
+		self.mL = DC(24, 23, 5)
+		# Motor Right
+		self.mR = DC(27, 22, 4)
+		# Servomotor
+		self.direction = PAPA()
 
-			"""
-			# Sensor "UltraSonic" Left
-			self.sL = UltraSonic(23, 21)
-			# Motor Left
-			self.mL = DC(24,23,4)  # 19->10 ; 18->24 ; 
-			# Motor Right
-			self.mR = DC(27,22,5)  # 17->17 ; 16->23 ;
-			# Servo Motor
-			#self.direction = PAPA()
-			"""
-		finally:
-			GPIO.cleanup()
+		"""
+		# Sensor "UltraSonic" Left
+		self.sL = UltraSonic(23, 21)
+		# Motor Left
+		self.mL = DC(24,23,4)  # 19->10 ; 18->24 ; 
+		# Motor Right
+		self.mR = DC(27,22,5)  # 17->17 ; 16->23 ;
+		# Servo Motor
+		#self.direction = PAPA()
+		"""
 	
 	def start(self):
 		try:
-			mbool = False
-			while True:
-				if(mbool):
-					self.direction.turn(right=True)
-				else:
-					self.direction.turn(left=True)
+			self.direction.position = self.direction.maxPulse
+			self.direction.update()
+			
+			time.sleep(0.5)
 
-				if(self.direction.position == self.direction.maxPulse):
-					mbool = True
-				if(self.direction.position == self.direction.minPulse):
-					mbool = False
+			self.mL.forward()
+			self.mL.setSpeed(100)
+			self.mR.forward()
+			self.mR.setSpeed(100)
+			
+			time.sleep(5)
+
+			self.mL.setSpeed(0)
+			self.mR.setSpeed(0)
+
+			time.sleep(0.5)
+
+			self.direction.position = self.direction.midPulse
+			self.direction.update()
+
+			time.sleep(0.5)
+
+			self.mL.forward()
+			self.mL.setSpeed(100)
+			self.mR.forward()
+			self.mR.setSpeed(100)
+
+			time.sleep(5)
+
+			self.mL.setSpeed(0)
+			self.mR.setSpeed(0)
+
+			time.sleep(0.5)
 		finally:
 			self.direction.reset()
+
+			self.mL.setSpeed(0)
+			self.mR.setSpeed(0)
--- files/main.py
+++ files/main.py
@@ -1,5 +1,7 @@
+import RPi.GPIO as GPIO
+
 from mClass.tuture import Car
 
 if __name__ == "__main__":
-    tuture = Car()
-    tuture.start()
+	tuture = Car()
+	tuture.start()
